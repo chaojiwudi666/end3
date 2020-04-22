4e9779fa317b2df26829d258fc7a7eef6a2cd9dd
dongshuhao <746564612@qq.com>
2020-04-22 21:54:04 +0800

    Save before ejecting

---
+++ end3/src/config/router.js
@@ -0,0 +1,17 @@
+const myRoute = [{
+  key: '/login',
+  name: 'login',
+  path: '/login',
+  exact: true,
+  component: 'views/Login',
+  model: 'store/login'
+},{
+  key: '/home',
+  name: 'home',
+  path: '/home',
+  exact: true,
+  component: 'views/home',
+  model: 'store/home'
+}]
+export default myRoute;
+//1.新页面在这里添加路由
